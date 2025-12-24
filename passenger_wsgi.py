2f7d5ed7b4887a51c5db68b82947eef39726a0e2
AandiaAbasi <andiaabasi2@gmail.com>
2025-12-24 23:26:02 +0330

    first commit

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,5 @@
 import os
-from fofofish import get_wsgi_application
+from fofofish.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fofofish.settings')
 application = get_wsgi_application()
