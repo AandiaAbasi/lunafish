603c939148630c72970393a4598ae94590433eba
AandiaAbasi <andiaabasi2@gmail.com>
2025-12-24 22:27:57 +0330

    first commit

--- fofofish/settings.py
+++ fofofish/settings.py
@@ -146,7 +146,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-
+AUTH_USER_MODEL = 'account.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
