97cc16734220417039e7c1dcf04a2694152a9e1e
AandiaAbasi <andiaabasi2@gmail.com>
2025-12-25 19:17:57 +0330

    first commit

--- fofofish/settings.py
+++ fofofish/settings.py
@@ -111,26 +111,26 @@ WSGI_APPLICATION = 'fofofish.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql', 
-#         'NAME': 'fofofish_fofofish', 
-#         'USER': 'fofofish_fofofish', 
-#         'PASSWORD': 'UZlbqUwJi)O5#0e#', 
-#         'HOST': '127.0.0.1', 
-#         'PORT': '3306', 
-#         'OPTIONS': {
-#             'charset': 'utf8mb4', 
-#         },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'fofofish_fofofish', 
+        'USER': 'fofofish_fofofish', 
+        'PASSWORD': 'UZlbqUwJi)O5#0e#', 
+        'HOST': '127.0.0.1', 
+        'PORT': '3306', 
+        'OPTIONS': {
+            'charset': 'utf8mb4', 
+        },
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
