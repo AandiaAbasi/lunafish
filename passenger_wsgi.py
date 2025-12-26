81199cf5dd7b693e05333c0a87d0554f4bcff394
AandiaAbasi <andiaabasi2@gmail.com>
2025-12-26 09:44:04 +0330

    first commit

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,11 @@
 import os
 import sys
-import django
 
 # Add the project directory to the Python path
 sys.path.insert(0, '/home/fofofish/fofofish')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fofofish.settings')
 
-# Prevent reentrant django.setup() calls
-if not django.apps.apps.ready:
-    from django.core.wsgi import get_wsgi_application
-    application = get_wsgi_application()
-else:
-    from fofofish.wsgi import application
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
