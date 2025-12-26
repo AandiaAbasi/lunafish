ec6d65b587e47c4b4e310bba461ccb9e043c8f15
AandiaAbasi <andiaabasi2@gmail.com>
2025-12-26 15:23:03 +0330

    first commit

--- account/translation.py
+++ account/translation.py
@@ -1,7 +1,11 @@
 from modeltranslation.translator import translator, TranslationOptions
 from .models import User
+from .admin import RegularUserProxy, TeacherUserProxy
 
 
 class UserTranslationOptions(TranslationOptions):
     fields = ('name', 'bio')
+
 translator.register(User, UserTranslationOptions)
+translator.register(RegularUserProxy, UserTranslationOptions)
+translator.register(TeacherUserProxy, UserTranslationOptions)
