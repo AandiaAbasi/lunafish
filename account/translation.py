0c122dc40d1ead75ff9958bec16db8d5553f6e17
AandiaAbasi <andiaabasi2@gmail.com>
2025-12-26 08:47:53 +0330

    first commit

--- account/translation.py
+++ account/translation.py
@@ -4,4 +4,5 @@ from .models import User
 
 class UserTranslationOptions(TranslationOptions):
     fields = ('name', 'bio')
+    empty_values = ('name', 'bio')
 translator.register(User, UserTranslationOptions)
--- core/translation.py
+++ core/translation.py
@@ -3,29 +3,36 @@ from .models import About, Term, Privacy, Contact, Article, FAQ, Banner
 
 
 class AboutTranslationOptions(TranslationOptions):
-    fields = ('title', 'content')  
+    fields = ('title', 'content')
+    empty_values = ('title', 'content')
 translator.register(About, AboutTranslationOptions)
 
 class TermTranslationOptions(TranslationOptions):
-    fields = ('title', 'content')  
+    fields = ('title', 'content')
+    empty_values = ('title', 'content')
 translator.register(Term, TermTranslationOptions)
 
 class PrivacyTranslationOptions(TranslationOptions):
-    fields = ('title', 'content')  
+    fields = ('title', 'content')
+    empty_values = ('title', 'content')
 translator.register(Privacy, PrivacyTranslationOptions)
 
 class ContactTranslationOptions(TranslationOptions):
-    fields = ('title', 'value')  
+    fields = ('title', 'value')
+    empty_values = ('title', 'value')
 translator.register(Contact, ContactTranslationOptions)
 
 class ArticleTranslationOptions(TranslationOptions):
-    fields = ('title', 'short_description', 'content')  
+    fields = ('title', 'short_description', 'content')
+    empty_values = ('title', 'short_description', 'content')
 translator.register(Article, ArticleTranslationOptions)
 
 class FAQTranslationOptions(TranslationOptions):
-    fields = ('question', 'answer')  
+    fields = ('question', 'answer')
+    empty_values = ('question', 'answer')
 translator.register(FAQ, FAQTranslationOptions)
 
 class BannerTranslationOptions(TranslationOptions):
-    fields = ('title', 'sub_title')  
+    fields = ('title', 'sub_title')
+    empty_values = ('title', 'sub_title')
 translator.register(Banner, BannerTranslationOptions)
