ba39d1a57894014d05ec7b321aa56b5ace830c62
AandiaAbasi <andiaabasi2@gmail.com>
2025-12-26 08:58:31 +0330

    first commit

--- account/translation.py
+++ account/translation.py
@@ -4,5 +4,4 @@ from .models import User
 
 class UserTranslationOptions(TranslationOptions):
     fields = ('name', 'bio')
-    empty_values = ('name', 'bio')
 translator.register(User, UserTranslationOptions)
