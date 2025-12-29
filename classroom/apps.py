46c67b717561ed98a8e7e25fa51cb5b0d76dead1
AandiaAbasi <andiaabasi2@gmail.com>
2025-12-29 16:31:48 +0330

    fatemeh-90-days

--- classroom/apps.py
+++ classroom/apps.py
@@ -5,3 +5,6 @@ class ClassroomConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'classroom'
     verbose_name = 'Classroom Management'
+    
+    def ready(self):
+        import classroom.translation
