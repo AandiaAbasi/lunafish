d642a80024a97522b7d4ccaabd22c9725c582018
AandiaAbasi <andiaabasi2@gmail.com>
2025-12-29 16:27:35 +0330

    fatemeh-90-days

--- classroom/admin.py
+++ classroom/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import TeacherAvailability, ClassSession, ClassEnrollment, ClassAnnouncement
+from .models import (
+    TeacherAvailability, TeachingSubject, DiscountCode, ClassBooking,
+    TeacherWallet, ClassRevenue, WithdrawalRequest, WalletTransaction,
+    StudentTransaction, PlatformSettings
+)
 
 
 @admin.register(TeacherAvailability)
